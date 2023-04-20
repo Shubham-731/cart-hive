d8e9b8f91123ea7602650d2ce7996bf2ba26fb63
Shubham P <shpcore103@gmail.com>
2023-04-20 14:21:51 +0530

    revert cors

--- server/src/app.ts
+++ server/src/app.ts
@@ -15,12 +15,7 @@ const CLIENT_URL: string = process.env.CLIENT_URL || "http://localhost:3000";
 const app: Express = express();
 
 // Middlewares
-app.use(
-  cors({
-    origin: CLIENT_URL,
-    credentials: true,
-  })
-);
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
